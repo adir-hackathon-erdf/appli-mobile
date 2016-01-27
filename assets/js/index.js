bc60a031c8a7418fa0cf91a40f928edd358d80e8
Renaud <renaud.vivancos@free.fr>
2016-01-27 08:49:02 +0100

    javascript fonctionnel + mise a jour de index.html + creation du fichier listePDL.html pour test du javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,15 @@
   var nom4 = document.getElementById('nom-pdl4');
   var pdl4 = document.getElementById('pdl-4');
 
+  var lieu1 = document.getElementById('lieu1');
+  var nb1 = document.getElementById('nb1');
+  var lieu2 = document.getElementById('lieu2');
+  var nb2 = document.getElementById('nb2');
+  var lieu3 = document.getElementById('lieu3');
+  var nb3 = document.getElementById('nb3');
+  var lieu4 = document.getElementById('lieu4');
+  var nb4 = document.getElementById('nb4');
+
   pdls.push(nom1);
   pdls.push(pdl1);
   pdls.push(nom2);
@@ -24,37 +33,36 @@
 
   function stockagePdls()
   {
-
+    var a = 1;
     for (i=0; i<pdls.length; i+=2)
     {
-      j = i + 1;
-
-      var nom_pdl = 'nom_pdl'+i.toString();
-      var num_pdl = 'num_pdl'+j.toString();
-
+      var nom_pdl = 'nom_pdl'+a.toString();
       localStorage.setItem(nom_pdl, pdls[i].value);
-      localStorage.setItem(num_pdl, pdls[j].value);
-
-      infoPdls.push(localStorage.getItem(nom_pdl));
-      infoPdls.push(localStorage.getItem(num_pdl));
-    
+      a++;
     }
 
+    var a = 1;
+    for (i=1; i<pdls.length; i+=2)
+    {
+      var num_pdl = 'num_pdl'+a.toString();
+      localStorage.setItem(num_pdl, pdls[i].value);
+      a++;
+    }
   }
   
   if (bouton != null)
   {
     bouton.addEventListener('click', stockagePdls, false);  
-    localStorage.clear();
   }
   else
-  {
-    console.log('Lieu 1 : '+localStorage.nom_pdl1);
-    console.log('Pdl 1 : '+localStorage.num_pdl1);
-    console.log('Lieu 2 : '+localStorage.nom_pdl2);
-    console.log('Pdl 2 : '+localStorage.num_pdl2);
-    console.log('Lieu 3 : '+localStorage.nom_pdl3);
-    console.log('Pdl 3 : '+localStorage.num_pdl3);
-    console.log('Lieu 4 : '+localStorage.nom_pdl4);
-    console.log('Pdl 4 : '+localStorage.num_pdl4);
+  { 
+    lieu1.innerHTML = localStorage.nom_pdl1;
+    nb1.innerHTML = localStorage.num_pdl1;
+    lieu2.innerHTML = localStorage.nom_pdl2;
+    nb2.innerHTML = localStorage.num_pdl2;
+    lieu3.innerHTML = localStorage.nom_pdl3;
+    nb3.innerHTML = localStorage.num_pdl3;
+    lieu4.innerHTML = localStorage.nom_pdl4;
+    nb4.innerHTML = localStorage.num_pdl4;
   }
+  
